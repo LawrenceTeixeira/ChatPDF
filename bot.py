b713f66962259ff2ce6b0972ea11989ad721fcc9
LawrenceTeixeira <43327665+LawrenceTeixeira@users.noreply.github.com>
2023-07-30 13:45:59 -0300

    modified:   bot.py 	new file:   botanterior.py 	modified:   functions.py

--- bot.py
+++ bot.py
@@ -1,44 +1,67 @@
-from flask import Flask, render_template, request
-from autocorrect import spell
+import openai
+import streamlit as st
 from functions import LoadPDF, Query, LoadIndex
 
-app = Flask(__name__)
-
-#BRAIN_FILE="./pretrained_model/aiml_pretrained_model.dump"
-#k = aiml.Kernel()
-
-#book_docsearch = LoadPDF()
 book_docsearch = LoadIndex()
+#book_docsearch = LoadPDF()
 
-#if os.path.exists(BRAIN_FILE):
-#    print("Loading from brain file: " + BRAIN_FILE)
- #   k.loadBrain(BRAIN_FILE)
-#else:
-#    print("Parsing aiml files")
-#    k.bootstrap(learnFiles="./pretrained_model/learningFileList.aiml", commands="load aiml")
-#    print("Saving brain file: " + BRAIN_FILE)
-#    k.saveBrain(BRAIN_FILE)
-
-
-@app.route("/")
-def home():
-    return render_template("home.html")
+st.set_page_config(
+    page_title="Procedures of the Brazilian Patent and Trademark Office (BRPTO)",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.gov.br/inpi/pt-br',
+        'About': "This app is designed to guide you through the fundamental procedures of safeguarding your trademarks, patents, industrial designs, geographical indications, computer programs, circuit topographies, and technology contracts with the Brazilian Patent and Trademark Office (BRPTO)."
+    }
+)
 
 
-@app.route("/get")
-def get_bot_response():
-    query = request.args.get('msg')
-   
+with st.sidebar:
+    st.image("https://lawrence.eti.br/wp-content/uploads/2023/07/Untitled-300-×-100-px-1.png")   
+    option = st.selectbox(
+    'Which version of the GPT model do you prefer to use?',
+    ('gpt-4', 'gpt-3.5-turbo' ),)
+    ""
+    "Official Manuals of the BRPTO:"
+    "[Tradmarks](https://lawrence.eti.br/wp-content/uploads/2023/07/Manual_de_Marcas_3a_edicao_6a_revisao.pdf)"
+    "[Patent](https://lawrence.eti.br/wp-content/uploads/2023/07/ManualdePatentes20210706-1.pdf)"
+    "[Industrial Designs](https://lawrence.eti.br/wp-content/uploads/2023/07/GuiadoUsurioDI.pdf)"
+    "[Geographical Indications](https://lawrence.eti.br/wp-content/uploads/2023/07/Manual_de_IG_1a_edicao_2a_revisao.pdf)"
+    "[Computer Programs](https://lawrence.eti.br/wp-content/uploads/2023/07/manual-e-software-2022.pdf)"
+    "[Circuit Topographies](https://lawrence.eti.br/wp-content/uploads/2023/07/ManualdoUsurioeChipportugusV1.2.1.pdf)"
+    "[Technology Contracts](https://lawrence.eti.br/wp-content/uploads/2023/07/manualcontratos.pdf)"
+    ""
+    "Source: [BRPTO](https://www-gov-br.translate.goog/inpi/pt-br?_x_tr_sl=pt&_x_tr_tl=en&_x_tr_hl=pt-BR&_x_tr_pto=wapp)"
+    ""
+    "Created by [Lawrence Teixeira](https://www.linkedin.com/in/lawrenceteixeira/)"
+    ""
+    ""
+    "Please note that this chatbot has no affiliation with the Brazilian Patent and Trademark Office (BRPTO)."
+st.title("💬 Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "Hey, 😊 how are you? Feel free to ask me questions in any language about the BRPTO's Official Manuals."}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+     
+    response = Query(prompt, book_docsearch, option)
     
-    response = Query(query, book_docsearch)
     if response:
-        return (str(response))
+        result = (str(response))
     else:
-        return (str(":)"))
+        result = (str(":)"))
 
 
-if __name__ == "__main__":
-    # app.run()
-    app.run(host='0.0.0.0', port=8080)
+    msg = { "role": "assistant",
+            "content": result
+    }
 
-
+    st.session_state.messages.append(msg)
+       
+    st.chat_message("assistant").write(msg["content"])
--- functions.py
+++ functions.py
@@ -3,9 +3,11 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
 from langchain.document_loaders import PyPDFDirectoryLoader
+from prettytable import PrettyTable
 
 # OpenAI API Key and settings
 OPENAI_API_KEY = "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"
@@ -46,23 +48,72 @@ def LoadPDF():
   if index_name not in pinecone.list_indexes():
       print("Index does not exist: ", index_name)
 
-  book_docsearch = Pinecone.from_texts([t.page_content for t in book_texts], embeddings, index_name = index_name)
+  book_docsearch = Pinecone.from_documents(book_texts, embeddings, index_name = index_name)
 
   return book_docsearch
 
-def Query(query, book_docsearch):
+def Query(query, book_docsearch, option):
   # Let's set up the query
   docs = book_docsearch.similarity_search(query)
 
+  #docs = book_docsearch.max_marginal_relevance_search(query, k=5, fetch_k=10)
+
   # set up the llm model for our qa session
-  llm = OpenAI(temperature=1, openai_api_key=OPENAI_API_KEY, model="text-davinci-003"  ) 
+  if option == "text-davinci-003":
+     llm = OpenAI(temperature=1, openai_api_key=OPENAI_API_KEY, model=option ) 
+  else :
+     llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0, model_name=option )
   
   # Run the QA chain with your query to get the answer
-  chain = load_qa_chain(llm, chain_type="stuff")
+  chain = load_qa_chain(llm,   chain_type="stuff")
   
-  retorno = chain.run(input_documents=docs, question=query)
+  print(docs)
 
-  return retorno
+  # Initialize pretty table with column names
+  table = PrettyTable(['Page', 'Source'])
+  
+  seen = set()
+  
+  rmanual = ""
+
+  for doc in docs:
+    page_content = doc.page_content
+    page = doc.metadata.get('page', 'N/A')  # Default to 'N/A' if 'page' is not available
+    source = doc.metadata.get('source', 'N/A')  # Default to 'N/A' if 'source' is not available
+    
+    if page != 'N/A':
+       # check if the page and source combination is unique
+       if (page, source) not in seen:
+
+           if source == "pdfs\ManualdePatentes20210706.pdf":
+              rmanual = rmanual + f", [Patents's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/ManualdePatentes20210706-1.pdf)" 
+           
+           elif source == "pdfs\Manual_de_Marcas_3ª_edicao_6ª_revisao.pdf":
+                rmanual = rmanual + f", [Tradmarks's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/Manual_de_Marcas_3a_edicao_6a_revisao.pdf)" 
+
+           elif source == "pdfs\GuiadoUsurioDI.pdf":
+                rmanual = rmanual + f", [Industrial Designs's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/GuiadoUsurioDI.pdf)" 
+
+           elif source == "pdfs\Manual_de_IG_1a_edicao_2a_revisao.pdf":
+                rmanual = rmanual + f", [Geographical Indications's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/Manual_de_IG_1a_edicao_2a_revisao.pdf)" 
+              
+           elif source == "pdfs\manual-e-software-2022.pdf":
+                rmanual = rmanual + f", [Computer Programs's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/manual-e-software-2022.pdf)" 
+            
+           elif source == "pdfs\ManualdoUsurioeChipportugusV1.2.1.pdf":
+                rmanual = rmanual + f", [Circuit Topographies's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/ManualdoUsurioeChipportugusV1.2.1.pdf)" 
+              
+           elif source == "pdfs\manualcontratos.pdf":
+                rmanual = rmanual + f", [Technology Contracts's manual](https://lawrence.eti.br/wp-content/uploads/2023/07/manualcontratos.pdf)" 
+         
+           table.add_row([page, source])
+           seen.add((page, source))  # Add the combination to the set of seen combinations
+      
+  print (table)
+
+  retorno = chain.run(input_documents=docs, question=query )
+
+  return retorno + " Source: " + rmanual.replace(", ", "", 1) + '.'
 
 def LoadIndex():
   # Set the index name for this project in pinecone first 
